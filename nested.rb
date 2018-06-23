42905ff0e2256a3c1a33560de01f05bc6972f524
Sean Conrad <seanconrad123@gmail.com>
2018-06-23 05:47:57 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -58,6 +58,7 @@ def dennis_ritchies_language
      }
 		 print programmer_hash[:dennis_ritchie][:languages]
 end
+dennis_ritchies_language
 
 def adding_matz
 # add the following information to the top level of programmer_hash
